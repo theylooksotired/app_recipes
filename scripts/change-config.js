08f5f3a8cb361cdb6ec6a6f6fd5582ca8ee3d5d8
Leano Martinet <theylooksotired@gmail.com>
2018-09-21 16:00:01 +0200

    New version

--- scripts/change-config.js
+++ scripts/change-config.js
@@ -35,7 +35,7 @@ for (let i=0; i < apps.length; i++) {
 				let result = data;
 				result = result.replace(/APP_TITLE/g, json.site.title);
 				result = result.replace(/APP_DESCRIPTION/g, json.site.description);
-				result = result.replace(/APP_VERSION/g, '4.0.2');
+				result = result.replace(/APP_VERSION/g, '4.0.3');
 				result = result.replace(/APP_ID/g, json.site.id);
 				fs.writeFile('config.xml', result, 'utf8', function (err) {
 					if (err) return console.log(err);

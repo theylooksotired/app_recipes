a4d54aeba766fc79bfaafa2bd4d12e113fac3116
Leano Martinet <theylooksotired@gmail.com>
2018-09-24 12:12:52 +0200

    New version

--- scripts/change-config.js
+++ scripts/change-config.js
@@ -9,7 +9,8 @@ const argv = require('minimist')(process.argv.slice(2));
 // pgb unlock android 132896
 // pgb clone 3302785 && pgb download 3302785 android
 
-let apps = [{country: 'uruguay', url: 'https://www.cocina-uruguaya.com/json-phonegap'},
+let apps = [{country: 'argentina', url: 'https://www.recetas-argentinas.com/json-phonegap'},
+			{country: 'uruguay', url: 'https://www.cocina-uruguaya.com/json-phonegap'},
             {country: 'colombia', url: 'https://www.cocina-colombiana.com/json-phonegap'}];
 
 let app = {};
